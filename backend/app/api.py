2b5a808f06a493500c2dcb4e38284e1ac1ee2dd6
boutillier-paul <boutillier.paul@gmail.com>
2023-02-23 16:50:01 +0100

    feat: more security checks on endpoints

--- backend/app/api.py
+++ backend/app/api.py
@@ -17,6 +17,7 @@ async def api_root():
 
 
 # REGISTER ENDPOINT
+
 @router.post("/signup", tags=["Register"])
 async def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user_email = controllers.get_user_by_email(db, user_email=user.email)
@@ -36,6 +37,7 @@ async def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 # LOGIN ENDPOINT
+
 @router.post("/login", tags=["Login"])
 async def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
     db_user = controllers.get_user_by_login(db, user_login=user.login)
@@ -52,19 +54,11 @@ async def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
             detail="Incorrect login or password",
         )
 
-
 # USER ENDPOINTS
+
 @router.get("/users/me", tags=["Users"], response_model=schemas.User, dependencies=[Depends(JWTBearer)])
 async def get_current_user(db: Session = Depends(get_db), Authorization: str = Header(None)):
-
-    token = Authorization.split(" ")[1]
-    decoded_token = security.decodeJWT(token)
-    if not decoded_token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, 
-            detail="Token expired"
-        )
-    db_user = controllers.get_user_by_login(db, user_login=decoded_token['user_login'])
+    db_user = controllers.get_current_user(db, Authorization=Authorization)
     return db_user
 
 @router.get("/users/{user_id}", tags=["Users"], response_model=schemas.User, dependencies=[Depends(JWTBearer())])
@@ -77,7 +71,7 @@ async def read_user(user_id: int, db: Session = Depends(get_db)):
         )
     return db_user
 
-@router.get("/users", tags=["Users", "ADMIN"], response_model=list[schemas.User], dependencies=[Depends(JWTBearer())])
+@router.get("/users", tags=["Users", "ADMIN ROLE"], response_model=list[schemas.User], dependencies=[Depends(JWTBearer())])
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), Authorization: str = Header(None)):
     controllers.check_user_role(db, role_name="ADMIN", Authorization=Authorization)
     db_users = controllers.get_users(db, skip=skip, limit=limit)
@@ -105,7 +99,7 @@ async def search_plant(plant_name: str, db: Session = Depends(get_db)):
         )
     return db_plants
 
-@router.get("/plants", tags=["Plants", "ADMIN"], response_model=list[schemas.Plant], dependencies=[Depends(JWTBearer())])
+@router.get("/plants", tags=["Plants", "ADMIN ROLE"], response_model=list[schemas.Plant], dependencies=[Depends(JWTBearer())])
 async def read_plants(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), Authorization: str = Header(None)):
     controllers.check_user_role(db, role_name="ADMIN", Authorization=Authorization)
     db_plants = controllers.get_plants(db, skip=skip, limit=limit)
@@ -122,13 +116,21 @@ async def create_plant(plant: schemas.PlantCreate, user_id: int, db: Session = D
     return db_plant
 
 @router.delete("/plants/{plant_id}", tags=["Plants"], response_model=schemas.Plant, dependencies=[Depends(JWTBearer())])
-async def delete_plant(plant_id: int, db: Session = Depends(get_db)):
+async def delete_plant(plant_id: int, db: Session = Depends(get_db), Authorization: str = Header(None)):
     db_plant = controllers.get_plant(db, plant_id=plant_id)
+    user = controllers.get_current_user(db, Authorization=Authorization)
+
     if db_plant is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
             detail="Plant not found"
         )
+    if db_plant not in user.plants:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Unauthorized"
+        )
+
     db_plant = controllers.delete_plant(db, plant_id=plant_id)
     return db_plant
     
@@ -145,9 +147,12 @@ async def create_guard(guard: schemas.GuardCreate, plant_id: int, db: Session = 
         )
     return db_guard
 
-@router.put("/guards/{guard_id}", tags=["Guards"], response_model=schemas.Guard, dependencies=[Depends(JWTBearer())])
-async def take_guard(guard_id: int, user_id: int, db: Session = Depends(get_db)):
+@router.put("/guards/{guard_id}", tags=["Guards", "BOTANIST ROLE"], response_model=schemas.Guard, dependencies=[Depends(JWTBearer())])
+async def take_guard(guard_id: int, db: Session = Depends(get_db), Authorization: str = Header(None)):
+    controllers.check_user_role(db, role_name="BOTANIST", Authorization=Authorization)
     db_guard = controllers.get_guard(db, guard_id)
+    user = controllers.get_current_user(db, Authorization=Authorization)
+
     if db_guard is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
@@ -158,17 +163,26 @@ async def take_guard(guard_id: int, user_id: int, db: Session = Depends(get_db))
             status_code=status.HTTP_400_BAD_REQUEST, 
             detail="Guard already taken"
         )
-    db_guard = controllers.take_guard(db, guard_id=guard_id, user_id=user_id)
+
+    for plant in user.plants:
+        if db_guard in plant.guards:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, 
+                detail="You can't take a guard you have created"
+            )
+
+    db_guard = controllers.take_guard(db, guard_id=guard_id, user_id=user.id)
     return db_guard
 
-@router.get("/guards", tags=["Guards", "ADMIN"], response_model=list[schemas.Guard], dependencies=[Depends(JWTBearer())])
+@router.get("/guards", tags=["Guards", "ADMIN ROLE"], response_model=list[schemas.Guard], dependencies=[Depends(JWTBearer())])
 async def read_guards(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), Authorization: str = Header(None)):
     controllers.check_user_role(db, role_name="ADMIN", Authorization=Authorization)
     db_guards = controllers.get_guards(db, skip=skip, limit=limit)
     return db_guards
 
-@router.get("/guards/open", tags=["Guards"], response_model=list[schemas.Guard], dependencies=[Depends(JWTBearer())])
-async def read_open_guards(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+@router.get("/guards/open", tags=["Guards", "BOTANIST ROLE"], response_model=list[schemas.Guard], dependencies=[Depends(JWTBearer())])
+async def read_open_guards(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), Authorization: str = Header(None)):
+    controllers.check_user_role(db, role_name="BOTANIST", Authorization=Authorization)
     db_guards = controllers.get_open_guards(db, skip=skip, limit=limit)
     return db_guards
 
@@ -191,4 +205,35 @@ async def read_guard(guard_id: int, db: Session = Depends(get_db), Authorization
 
     return db_guard
 
+@router.put("/guards/{guard_id}", tags=["Guards"], response_model=schemas.Guard, dependencies=[Depends(JWTBearer())])
+async def cancel_guard(guard_id: int, user_id: int, db: Session = Depends(get_db), Authorization: str = Header(None)):
+    db_guard = controllers.cancel_guard(db, guard_id)
+    user = controllers.get_current_user(db, Authorization=Authorization)
+    
+    if db_guard is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Guard not found"
+        )
 
+    if db_guard not in user.guards or not db_guard.user_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Unauthorized"
+        )
+        
+    db_guard = controllers.take_guard(db, guard_id=guard_id, user_id=user_id)
+    return db_guard
+
+
+# SESSION ENDPOINTS
+
+@router.post("/sessions", tags=["Sessions"], response_model=schemas.CareSession, dependencies=[Depends(JWTBearer())])
+async def create_session(session: schemas.CareSessionCreate, guard_id: int, db: Session = Depends(get_db)):
+    db_session = controllers.create_care_session(db, care_session=session, guard_id=guard_id)
+    if db_session is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Session not created"
+        )
+    return db_session
