14c1ed7dad8f3e2dea2feca621403719030a9b87
Boutillier Paul <boutillier.paul@gmail.com>
2023-02-25 21:14:26 +0100

    fix: Care_Session models

--- backend/app/models.py
+++ backend/app/models.py
@@ -60,7 +60,7 @@ class Guard(Base):
 
     created_at = Column(DateTime, nullable=False, default=func.now())
 
-    care_sessions = relationship("CareSession", back_populates="guard")
+    care_sessions = relationship("Care_Session", back_populates="guard")
 
 
 class Care_Session(Base):
