d61a0b5e03543cc387ee150cb9bf65c942156921
alexis.looten <alexis.looten@pingflow.fr>
2023-02-26 13:11:59 +0100

    feat: begin locustfile

--- backend/locustfile.py
+++ backend/locustfile.py
@@ -1,20 +1,54 @@
+"""
+    LOCUSTFILE
+"""
+from main import app
+from app import schemas
+import json
+import random
+import string
 from locust import HttpUser, between, task
+from fastapi.testclient import TestClient
+
+
+client = TestClient(app)
 
 
 class Scenario_full_task(HttpUser):
     wait_time = between(1,10)
+    
+    # use string.hexdigits if whitespace not supported | printable
+    RANDOM_STR = ''.join(random.choices(string.hexdigits ,k=12))
 
-    # nbTrainer: int = 0
+    user_register = schemas.UserCreate(last_name=RANDOM_STR,
+                                first_name=RANDOM_STR,
+                                email=f"{RANDOM_STR}@gmail.com",
+                                login=RANDOM_STR,
+                                password=RANDOM_STR)
 
-    # id_trainer1: int 
-    # id_trainer2: int 
-
-    # api_id_pokemon1: int = 94
-    # api_id_pokemon2: int = 32
+    user_login = schemas.UserLogin(login=RANDOM_STR, password=RANDOM_STR)
+    
+    user_update = schemas.UserUpdate(last_name="John",
+                                first_name="Dhoe",
+                                email=f"{RANDOM_STR}@updated.fr")
+    user = schemas.User
+    
+    token: str
     
     @task
     def index(self):
-
+        
+        signup_response = client.post("/api/signup", content=json.dumps(self.user_register.__dict__))
+        login_response = client.post("/api/login", content=json.dumps(self.user_login.__dict__))
+        
+        self.token = login_response.json()['access_token']
+        
+        me_response = client.get("/api/users/me", headers={"Authorization": f"Bearer {self.token}"})
+        user_json = json.loads(me_response.json())
+        self.user = schemas.User(**user_json)
+        
+        update_me_response = client.put("/api/users/me", headers={"Authorization": f"Bearer {self.token}"}, content=json.dumps(self.user_update.__dict__))
+        
+        
         # self.nbTrainer = len(self.client.get("trainers").json())
 
         # # Create first new player 
