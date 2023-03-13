4b634ecd1c96e7384323f05eecb4744ccf12e1d9
Boutillier Paul <boutillier.paul@gmail.com>
2023-03-13 20:38:55 +0100

    fix: package.json conflict

--- frontend/src/app/pages/advices/advices.page.ts
+++ frontend/src/app/pages/advices/advices.page.ts
@@ -63,7 +63,7 @@ export class AdvicesPage {
     this.router.navigate(['/advice-click']);
   }
 
-  getObjectPhoto(photoName: string): Observable<string> {
+  getObjectPhoto(photoName: string) {
     return this.s3Service.getObjectFromS3(photoName);
   }
 }
